967a4ea1e4ab6337b19483376cce2f1d817f6011
Amit Agarwal <amitfce@iitr.ac.in>
2019-07-01 16:01:51 +0530

    TSAAS -> TSaaS

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -6,7 +6,7 @@ const logo=props=>(
         <img src={Logo}></img>
         <div style={{fontSize:props.fontSize}} className={classes.LogoText}>
         <p style={{whiteSpace:'nowrap'}}>Traffic Survey</p>
-        <p style={{whiteSpace:'nowrap'}}>As A Service</p>
+        <p style={{whiteSpace:'nowrap'}}>as a Service</p>
         </div>
     </div>
 )
