735000dbbb14af5e3eb4a440ad12db1f466c5a12
Amit Agarwal <amitfce@iitr.ac.in>
2019-07-04 12:16:07 +0530

    fixing conflicts and using gmaps on contact us page

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -21,25 +21,25 @@ class ContactUs extends Component{
             count:0
         }
     }
-    componentDidMount() {
-        this.platform = new window.H.service.Platform(this.state);
-        this.layer = this.platform.createDefaultLayers();
-        this.container = document.getElementById('civilmap');
-        this.map = new window.H.Map(this.container, this.layer.normal.map, {
-            center: this.state.center,
-            zoom: this.state.zoom,
-          })
-          this.group = new window.H.map.Group();
-        var events = new window.H.mapevents.MapEvents(this.map);
-        // eslint-disable-next-line
-        this. behavior = new window.H.mapevents.Behavior(events);
-        // eslint-disable-next-line
-        var ui = new window.H.ui.UI.createDefault(this.map,this.layer)
-        //this.addMarkersToMap(this.map,behavior);
-        //this.req(this.behavior);
-        this.map.addObject(this.group);
-        this.addMarkersToMap(this.state.center,this.behavior);     
-    }
+    // componentDidMount() {
+    //     this.platform = new window.H.service.Platform(this.state);
+    //     this.layer = this.platform.createDefaultLayers();
+    //     this.container = document.getElementById('civilmap');
+    //     this.map = new window.H.Map(this.container, this.layer.normal.map, {
+    //         center: this.state.center,
+    //         zoom: this.state.zoom,
+    //       })
+    //       this.group = new window.H.map.Group();
+    //     var events = new window.H.mapevents.MapEvents(this.map);
+    //     // eslint-disable-next-line
+    //     this. behavior = new window.H.mapevents.Behavior(events);
+    //     // eslint-disable-next-line
+    //     var ui = new window.H.ui.UI.createDefault(this.map,this.layer)
+    //     //this.addMarkersToMap(this.map,behavior);
+    //     //this.req(this.behavior);
+    //     this.map.addObject(this.group);
+    //     this.addMarkersToMap(this.state.center,this.behavior);     
+    // }
     addMarkersToMap=(position,behavior)=>{
         // if(this.props.initLat!==null&&this.props.initLng!==null)
         // {
@@ -65,9 +65,13 @@ class ContactUs extends Component{
                 <div className={classes.ContactMain}>
                 <div className={classes.ContactLeftMain}>
                     <h2>Office Location</h2>
-                    <div style={window.width>500?{width:'400px', height:'400px'}:{width:'100%', height:'400px'}} id="civilmap"></div>
+                    <div className={classes.GoogleMapResponsive}>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3460.051595087141!2d77.89629045133779!3d29.86278583399619!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390eb36f2388a651%3A0x7f4055c136ddbd53!2sDepartment+of+Civil+Engineering!5e0!3m2!1sen!2sin!4v1551327440327" width="600" height="450" frameborder="0" style={{border:"0"}} allowfullscreen></iframe>
+                </div>  
                 </div>
                 <div className={classes.ContactRightMain}>
+                    <p>Dr. Amit Agarwal</p>
+                    <p>Assistant Professor</p>
                     <p>Transportation Engineering Group</p>
                     <p>Department of Civil Engineering</p>
                     <p>IIT Roorkee</p>
--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -40,21 +40,11 @@ class Input extends Component {
                 
                 let inputElement1=this.props.elementconfig.options.map((option,index)=>{
                     return (
-<<<<<<< HEAD
-                        <div className={classes.RadioWrapper} key={index}>
-                            <label name={this.props.label} >{option.displayValue}</label>
-                            <input name={this.props.label} type={this.props.elementconfig.type} onChange={this.props.changed} value={option.value}  >
-          
-                        </input>
-=======
-                        <div  key={index}>
+                        <div className={classes.RadioWrapper}  key={index}>
                             <input name={this.props.label} type={this.props.elementconfig.type} onChange={this.props.changed} value={option.value} ></input>
                             <label name={this.props.label} > {option.displayValue}</label>
->>>>>>> eb4f1f8d85b3fbbf2581b581e009e32f3429ad85
                         </div>)
-
                         }
-                               
                 )
                 inputElement= <div style={{display:'flex',justifyContent:'space-around'}}>{inputElement1}</div>
                 // inputElement=<input onChange={this.props.changed} 
