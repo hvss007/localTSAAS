c183eb5114fc2169342805162197f511ab525eb7
Amit Agarwal <amitfce@iitr.ac.in>
2019-07-27 19:35:34 +0530

    updating wiki page

--- src/Components/Wiki/Wiki.js
+++ src/Components/Wiki/Wiki.js
@@ -1,4 +1,19 @@
 import React, { Component } from 'react';
+import FamilyScreenshot1 from '../../assets/screenshots/family1.png';
+import FamilyScreenshot2 from '../../assets/screenshots/family2.png';
+import FamilyScreenshot3 from '../../assets/screenshots/family3.png';
+import MemberScreenshot1 from '../../assets/screenshots/member1.png';
+import MemberScreenshot2 from '../../assets/screenshots/member2.png';
+import MemberScreenshot3 from '../../assets/screenshots/member3.png';
+import MemberScreenshot4 from '../../assets/screenshots/member4.png';
+import TripScreenshot1 from '../../assets/screenshots/trip1.png';
+import TripScreenshot2 from '../../assets/screenshots/trip2.png';
+import TripScreenshot3 from '../../assets/screenshots/trip3.png';
+import TripScreenshot4 from '../../assets/screenshots/trip4.png';
+import TripScreenshot5 from '../../assets/screenshots/trip5.png';
+import TripScreenshot6 from '../../assets/screenshots/trip6.png';
+import TripScreenshot7 from '../../assets/screenshots/trip7.png';
+
 // import Toolbar from '../StartSuvey/Navigation/Toolbar/Toolbar';
 // import SideDrawer from '../StartSuvey/SideDrawer/SideDrawer';
 import classes from './Wiki.css';
@@ -38,9 +53,36 @@ class  Wiki extends Component{
                 <h4> Data Collection: </h4>
                 <p>  In short, information about activities (purpose of the trip; e.g. home, work, education, social visits, leisure, shopping etc.) and trips (vehicle, trip distance, trip time etc.) on a typical day over a 24-hour period is recorded using this questionnaire. Further, detailed information of each screen is provided: </p>
                 <ul>
-                	<li>Family Information:</li>
-                	<li>Member Information:</li>
-                	<li>Trip Information:</li>
+                	<li>Family page:
+                        <ul>
+                            <li>Enter number of cars, scooter/motorcycles and bicycles. <Link to="/FamilyScreenshot1">See an example.</Link></li>
+                            <li>Enter monthly income of the family. <Link to="/FamilyScreenshot2">See an example.</Link></li>
+                            <li>Enter permanent address (home town): choose state and district, enter initials of nearest landmark and select one item from drop down landmarks. Finally drag the marker closest to your permanent address. <Link to="/FamilyScreenshot3">See an example.</Link></li>
+                            <li>Hint: If no drop down item matches your criteria, search with some other.</li>
+                            <li>Click on submit button.</li>
+                        </ul>
+                    </li>
+                    {/* <div> <span><img style={{width:'250px'}} alt={"family_screenshot1"} src={FamilyScreenshot1}></img></span><p>Family page</p></div>
+                    <div> <span><img style={{width:'250px'}} alt={"family_screenshot2"} src={FamilyScreenshot2}></img></span><p>Family page contd.</p></div>
+                    <div> <span><img style={{width:'250px'}} alt={"family_screenshot3"} src={FamilyScreenshot3}></img></span><p>Family page contd.</p></div> */}
+                	<li>Member page:</li>
+                    <ul>
+                        <li>Enter gender, age, educational qualification, maritial status, monthly income and principal source of income. <Link to="/MemberScreenshot1">See an example.</Link></li>
+                        <li>Enter number of sim cards, mobile phone usages while driving/traveling, licensing etc. <Link to="/MemberScreenshot2">See an example.</Link></li>
+                        <li>Enter if you are respondent and whether if you stay at home for the whole day and click on submit.<Link to="/MemberScreenshot3">See an example.</Link></li>
+                        <li>If the member stay at the home, a warning will appear. Click on 'Ok' or 'Yes' if this member is last member of family, this will end the survey. Else click on 'Cancel' or 'No', this will send you to the trip page. <Link to="/MemberScreenshot4">See an example.</Link></li>
+                    </ul>
+                	<li>Trip page:</li>
+                    <ul>
+                        <li>Enter the state and district in which a trip is made. <Link to="/TripScreenshot1">See an example.</Link></li>
+                        <li>Enter the landmark nearest to your origin location. This will show a map with a marker. Drag the marker nearest to your origin location and click 'Ok'. <Link to="/TripScreenshot2">See an example.</Link></li>
+                        <li>Enter the type of origin and appeoximate departure time. <Link to="/TripScreenshot3">See an example.</Link></li>
+                        <li>Similar to origin, enter the landmark nearest to your destination, adjust the marker, click on 'Ok', select type of destination type and appeoximate arrival time. <Link to="/TripScreenshot4">See an example.</Link></li>
+                        <li>Enter the main travel mode (vehicle/walk) which the member take to reach destination, enter travel mode (or walk) used to access/egree the main travel mode.<Link to="/TripScreenshot5">See an example.</Link></li>
+                        <li>Enter the travel time, travel cost and travel distance for the whole trip. Click on 'Add Trip'.<Link to="/TripScreenshot6">See an example.</Link></li>
+                        <li>This will start a new trip, origin information of second trip onwards will be auto filled. Start entering details with departure time.</li>
+                        <li>Click on 'Remove trip' to remove a trip. Click on 'Add Member' if all trips of the current member are entered. Click on 'Finish Survey' only if information of all members are entered. <Link to="/TripScreenshot7">See an example.</Link></li>
+                    </ul>
                 </ul>
  				
                  <h4> Privacy Policy:</h4>
