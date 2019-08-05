ae827cd537ab2e680fc34ff10eafa6248c82b6ae
Harsh Vardhan <hvss007@gmail.com>
2019-08-05 17:17:57 +0530

    Others selected on family page confirm box

--- src/Hoc/Backdrop/Backdrop1.js
+++ src/Hoc/Backdrop/Backdrop1.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import classes from './Backdrop1.css'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 const backdrop1=(props)=>{
     
     return(
        props.show? <div onClick={()=>{
            props.hideModalBackdrop(!props.show);
         }}  className={classes.Backdrop1}>
-            {props.src?<img alt="example" src={props.src}></img>:null}
+            {props.alert?props.children:null}
+            {props.src?<LazyLoadImage alt="example" src={props.src}/>:null}
         </div>:null
     )
 }
