2023821f4095997688e8c43e1c2700903f215155
SiddharthVarshney <siddharth.98varshney@gmail.com>
2019-08-26 20:53:04 +0530

    complete rating form

--- src/Components/PTSurvey/RatingForm/RatingForm.js
+++ src/Components/PTSurvey/RatingForm/RatingForm.js
@@ -3,6 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import RatingStars from "./../../RatingStars/RatingStars";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
+import axios from "axios";
+import HostName from "./../../../assets/globalvaribles/GlobalVariables";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -11,68 +14,404 @@ const useStyles = makeStyles(theme => ({
     "@media (max-width:1024px)": {
       margin: theme.spacing(2, 2)
     }
+  },
+  button: {
+    justifyContent: "center",
+    margin: "0 15vw 2vh 15vw",
+    textAlign: "center"
   }
 }));
 
-export default function RatingForm() {
+export default function RatingForm(props) {
   const classes = useStyles();
+  const [racc1, setRacc1] = React.useState(0);
+  const [racc2, setRacc2] = React.useState(0);
+  const [racc3, setRacc3] = React.useState(0);
+
+  const [rtrav1, setRtrav1] = React.useState(0);
+  const [rtrav2, setRtrav2] = React.useState(0);
+  const [rtrav3, setRtrav3] = React.useState(0);
+  const [rtrav4, setRtrav4] = React.useState(0);
+  const [rtrav5, setRtrav5] = React.useState(0);
+  const [rtrav6, setRtrav6] = React.useState(0);
+  const [rtrav7, setRtrav7] = React.useState(0);
+  const [rtrav8, setRtrav8] = React.useState(0);
+
+  const [rmov1, setRmov1] = React.useState(0);
+  const [rmov2, setRmov2] = React.useState(0);
+  const [rmov3, setRmov3] = React.useState(0);
+  const [rmov4, setRmov4] = React.useState(0);
+  const [rmov5, setRmov5] = React.useState(0);
+  const [rmov6, setRmov6] = React.useState(0);
+  const [rmov7, setRmov7] = React.useState(0);
+  const [rmov8, setRmov8] = React.useState(0);
+  const [rmov9, setRmov9] = React.useState(0);
+  const [rmov10, setRmov10] = React.useState(0);
+
+  const [rcom1, setRcom1] = React.useState(0);
+  const [rcom2, setRcom2] = React.useState(0);
+  const [rcom3, setRcom3] = React.useState(0);
+  const [rcom4, setRcom4] = React.useState(0);
+  const [rcom5, setRcom5] = React.useState(0);
+  const [rcom6, setRcom6] = React.useState(0);
+  const [rcom7, setRcom7] = React.useState(0);
+  const [rcom8, setRcom8] = React.useState(0);
+  const [rcom9, setRcom9] = React.useState(0);
+  const [rcom10, setRcom10] = React.useState(0);
+
+  const [rsec1, setRsec1] = React.useState(0);
+  const [rsec2, setRsec2] = React.useState(0);
+  const [rsec3, setRsec3] = React.useState(0);
+  const [rsec4, setRsec4] = React.useState(0);
+
+  const [rover1, setRover1] = React.useState(0);
+  const [rover2, setRover2] = React.useState(0);
+
+  function handleSubmit() {
+    const data = {
+      racc1: racc1,
+      racc2: racc2,
+      racc3: racc3,
+
+      rtrav1: rtrav1,
+      rtrav2: rtrav2,
+      rtrav3: rtrav3,
+      rtrav4: rtrav4,
+      rtrav5: rtrav5,
+      rtrav6: rtrav6,
+      rtrav7: rtrav7,
+      rtrav8: rtrav8,
+
+      rmov1: rmov1,
+      rmov2: rmov2,
+      rmov3: rmov3,
+      rmov4: rmov4,
+      rmov5: rmov5,
+      rmov6: rmov6,
+      rmov7: rmov7,
+      rmov8: rmov8,
+      rmov9: rmov9,
+      rmov10: rmov10,
+
+      rcom1: rcom1,
+      rcom2: rcom3,
+      rcom3: rcom2,
+      rcom4: rcom4,
+      rcom5: rcom5,
+      rcom6: rcom6,
+      rcom7: rcom7,
+      rcom8: rcom8,
+      rcom9: rcom9,
+      rcom10: rcom10,
+
+      rsec1: rsec1,
+      rsec2: rsec2,
+      rsec3: rsec3,
+      rsec4: rsec4,
+
+      rover1: rover1,
+      rover2: rover2
+    };
+
+    console.log("data", data);
+
+    axios.defaults.xsrfCookieName = "csrftoken";
+    axios.defaults.xsrfHeaderName = "X-CSRFToken";
+    axios.post(HostName + "ptSurveyRating/", data).then(Response => {
+      props.history.push({
+        pathname: "/"
+      });
+    });
+  }
   return (
     <div>
       <Card raised className={classes.root}>
         <Typography variant="h4" align="center">
           Accessibility Ratings:
         </Typography>
-        <RatingStars ratingQuestion="Convenience and ease to acces the interchange" />
-        <RatingStars ratingQuestion="Availability of fix Pick and drop places for different modes" />
-        <RatingStars ratingQuestion="Presence of sequence entry and exit points for arrival and departure passengers" />
+        <RatingStars
+          ratingQuestion="Convenience and ease to acces the interchange"
+          value={racc1}
+          onChange={(event, value) => {
+            setRacc1(value);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Availability of fix Pick and drop places for different modes"
+          value={racc2}
+          onChange={(event, value) => {
+            setRacc2(value);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Presence of sequence entry and exit points for arrival and departure passengers"
+          value={racc3}
+          onChange={(event, value) => {
+            setRacc3(value);
+          }}
+        />
         <Typography variant="h4" align="center">
           Way finding and travel Information Rating:
         </Typography>
-        <RatingStars ratingQuestion="Availability of information Signs of different tranport services, facilities and tranfers" />
-        <RatingStars ratingQuestion="Clarity in understanding of informatory signs" />
-        <RatingStars ratingQuestion="Availability of time table and travel information related to different modes" />
-        <RatingStars ratingQuestion="Presence of information screens fro time table and travel information in required language" />
-        <RatingStars ratingQuestion="Accuracy and reliability among different modes" />
-        <RatingStars ratingQuestion="Schedule coordination among different modes" />
-        <RatingStars ratingQuestion="Frequency of the modes within the city (intra-city)" />
-        <RatingStars ratingQuestion="Frequency of the modes outside the city (inter-city)" />
+        <RatingStars
+          ratingQuestion="Availability of information Signs of different tranport services, facilities and tranfers"
+          value={rtrav1}
+          onChange={(event, rtrav1) => {
+            setRtrav1(rtrav1);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Clarity in understanding of informatory signs"
+          value={rtrav2}
+          onChange={(event, rtrav2) => {
+            setRtrav2(rtrav2);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Availability of time table and travel information related to different modes"
+          value={rtrav3}
+          onChange={(event, rtrav3) => {
+            setRtrav3(rtrav3);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Presence of information screens fro time table and travel information in required language"
+          value={rtrav4}
+          onChange={(event, rtrav4) => {
+            setRtrav4(rtrav4);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Accuracy and reliability among different modes"
+          value={rtrav5}
+          onChange={(event, rtrav5) => {
+            setRtrav5(rtrav5);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Schedule coordination among different modes"
+          value={rtrav6}
+          onChange={(event, rtrav6) => {
+            setRtrav6(rtrav6);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Frequency of the modes within the city (intra-city)"
+          value={rtrav7}
+          onChange={(event, rtrav7) => {
+            setRtrav7(rtrav7);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Frequency of the modes outside the city (inter-city)"
+          value={rtrav8}
+          onChange={(event, rtrav8) => {
+            setRtrav8(rtrav8);
+          }}
+        />
         <Typography variant="h4" align="center">
           Transfer and Movement Ratings:
         </Typography>
-        <RatingStars ratingQuestion="Satisfaction related to transfer distance between differet modes" />
-        <RatingStars ratingQuestion="Presence of number of escalators, walkways and lifts" />
-        <RatingStars ratingQuestion="Presence of facilities for people with disablities" />
-        <RatingStars ratingQuestion="Presence of facilities for people with luggage" />
-        <RatingStars ratingQuestion="Satisfaction related to the distance between the parking area and modes" />
-        <RatingStars ratingQuestion="Availability of spaces for parking of vehicles" />
-        <RatingStars ratingQuestion="Availability of enquery countries" />
-        <RatingStars ratingQuestion="Presence of enquiry counters" />
-        <RatingStars ratingQuestion="Satisfaction related to time used in queuing and security check" />
-        <RatingStars ratingQuestion="Satisfaction from Behaviour and Assistance by the staff" />
+        <RatingStars
+          ratingQuestion="Satisfaction related to transfer distance between differet modes"
+          value={rmov1}
+          onChange={(event, rmov1) => {
+            setRmov1(rmov1);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Presence of number of escalators, walkways and lifts"
+          value={rmov2}
+          onChange={(event, rmov2) => {
+            setRmov2(rmov2);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Presence of facilities for people with disablities"
+          value={rmov3}
+          onChange={(event, rmov3) => {
+            setRmov3(rmov3);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Presence of facilities for people with luggage"
+          value={rmov4}
+          onChange={(event, rmov4) => {
+            setRmov4(rmov4);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Satisfaction related to the distance between the parking area and modes"
+          value={rmov5}
+          onChange={(event, rmov5) => {
+            setRmov5(rmov5);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Availability of spaces for parking of vehicles"
+          value={rmov6}
+          onChange={(event, rmov6) => {
+            setRmov6(rmov6);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Availability of enquery countries"
+          value={rmov7}
+          onChange={(event, rmov7) => {
+            setRmov7(rmov7);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Presence of enquiry counters"
+          value={rmov8}
+          onChange={(event, rmov8) => {
+            setRmov8(rmov8);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Satisfaction related to time used in queuing and security check"
+          value={rmov9}
+          onChange={(event, rmov9) => {
+            setRmov9(rmov9);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Satisfaction from Behaviour and Assistance by the staff"
+          value={rmov10}
+          onChange={(event, rmov10) => {
+            setRmov10(rmov10);
+          }}
+        />
         <Typography variant="h4" align="center">
           Comfort and Convenience Rating:
         </Typography>
-        <RatingStars ratingQuestion="Availability of facilities like different varieties of shops, waiting areas, restaurants, toilets, protection from weather, ATMs, etc" />
-        <RatingStars ratingQuestion="Availability of total number of seats inside the travel modes nad waiting areas" />
-        <RatingStars ratingQuestion="Availability of Wi-Fi, mobilephone and internet signals" />
-        <RatingStars ratingQuestion="Availability of total number of charging points inside the travel modes and waiting areas" />
-        <RatingStars ratingQuestion="Facilities provided for infants, children nad pregnant women" />
-        <RatingStars ratingQuestion="Facilities for elderly people" />
-        <RatingStars ratingQuestion="Noise level" />
-        <RatingStars ratingQuestion="Air quality" />
-        <RatingStars ratingQuestion="Presence of amount of dustbins (seperate for wet and dry waste)" />
-        <RatingStars ratingQuestion="Overall cleanliness" />
+        <RatingStars
+          ratingQuestion="Availability of facilities like different varieties of shops, waiting areas, restaurants, toilets, protection from weather, ATMs, etc"
+          value={rcom1}
+          onChange={(event, rcom1) => {
+            setRcom1(rcom1);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Availability of total number of seats inside the travel modes nad waiting areas"
+          value={rcom1}
+          onChange={(event, rcom2) => {
+            setRcom2(rcom2);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Availability of Wi-Fi, mobilephone and internet signals"
+          value={rcom3}
+          onChange={(event, rcom3) => {
+            setRcom3(rcom3);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Availability of total number of charging points inside the travel modes and waiting areas"
+          value={rcom4}
+          onChange={(event, rcom4) => {
+            setRcom4(rcom4);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Facilities provided for infants, children nad pregnant women"
+          value={rcom5}
+          onChange={(event, rcom5) => {
+            setRcom5(rcom5);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Facilities for elderly people"
+          value={rcom6}
+          onChange={(event, rcom6) => {
+            setRcom6(rcom6);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Noise level"
+          value={rcom7}
+          onChange={(event, rcom7) => {
+            setRcom7(rcom7);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Air quality"
+          value={rcom8}
+          onChange={(event, rcom8) => {
+            setRcom8(rcom8);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Presence of amount of dustbins (seperate for wet and dry waste)"
+          value={rcom9}
+          onChange={(event, rcom9) => {
+            setRcom9(rcom9);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Overall cleanliness"
+          value={rcom10}
+          onChange={(event, rcom10) => {
+            setRcom10(rcom10);
+          }}
+        />
         <Typography variant="h4" align="center">
           Safety and Security Rating:
         </Typography>
-        <RatingStars ratingQuestion="Safety and security in the waiting areas inside transport modes" />
-        <RatingStars ratingQuestion="Safety and security in transfer areas" />
-        <RatingStars ratingQuestion="Safety and security while getting on and off transport" />
-        <RatingStars ratingQuestion="Availability of CCTVs cameras and security guards" />
+        <RatingStars
+          ratingQuestion="Safety and security in the waiting areas inside transport modes"
+          value={rsec1}
+          onChange={(event, rsec1) => {
+            setRsec1(rsec1);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Safety and security in transfer areas"
+          value={rsec2}
+          onChange={(event, rsec2) => {
+            setRsec2(rsec2);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Safety and security while getting on and off transport"
+          value={rsec3}
+          onChange={(event, rsec3) => {
+            setRsec3(rsec3);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Availability of CCTVs cameras and security guards"
+          value={rsec4}
+          onChange={(event, rsec4) => {
+            setRsec4(rsec4);
+          }}
+        />
         <hr />
 
-        <RatingStars ratingQuestion="Overall rating of Anand Vihar as a multimodal transport hub" />
-        <RatingStars ratingQuestion="Rating of overall development around Anand Vihar to multimodal hub" />
+        <RatingStars
+          ratingQuestion="Overall rating of Anand Vihar as a multimodal transport hub"
+          value={rover1}
+          onChange={(event, rover1) => {
+            setRover1(rover1);
+          }}
+        />
+        <RatingStars
+          ratingQuestion="Rating of overall development around Anand Vihar to multimodal hub"
+          value={rover2}
+          onChange={(event, rover2) => {
+            setRover2(rover2);
+          }}
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={handleSubmit}
+          size="large"
+        >
+          Submit
+        </Button>
       </Card>
     </div>
   );
