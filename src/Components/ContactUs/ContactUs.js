1bda16846d80a74c4995c03f72c2a6c5de7028d5
Amit Agarwal <amitfce@iitr.ac.in>
2020-04-19 13:53:36 +0530

    change of webpage and added civil dept website navigation

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -66,10 +66,11 @@ class ContactUs extends Component{
                 <div className={classes.ContactLeftMain}>
                     <h2>Office Location</h2>
                     <div className={classes.GoogleMapResponsive}>
-                    <iframe title="IITR" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3460.051595087141!2d77.89629045133779!3d29.86278583399619!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390eb36f2388a651%3A0x7f4055c136ddbd53!2sDepartment+of+Civil+Engineering!5e0!3m2!1sen!2sin!4v1551327440327" width="600" height="450" frameborder="0" style={{border:"0"}} allowfullscreen></iframe>
+                    <iframe title="IITR" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3460.051595087141!2d77.89629045133779!3d29.86278583399619!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390eb36f2388a651%3A0x7f4055c136ddbd53!2sDepartment+of+Civil+Engineering!5e0!3m2!1sen!2sin!4v1551327440327" width="400" height="300" frameborder="0" style={{border:"0"}} allowfullscreen></iframe>
                 </div>  
                 </div>
                 <div className={classes.ContactRightMain}>
+                    <h3>
                     <p>Dr. Amit Agarwal</p>
                     <p>Assistant Professor</p>
                     <p>Transportation Engineering Group</p>
@@ -77,9 +78,10 @@ class ContactUs extends Component{
                     <p>IIT Roorkee</p>
                     <p>Roorkee, India-247667</p>
                     <p>Phone: +91-1332-285441</p>
-                    <a href="mailto:amitfce@iitr.ac.in"> amitfce@iitr.ac.in</a>
-                    <a href="http://faculty.iitr.ac.in/~amitfce/">http://faculty.iitr.ac.in/~amitfce/</a>
-                    <a href="https://www.iitr.ac.in/~CE/amitfce">https://www.iitr.ac.in/~CE/amitfce</a>
+                    <p><a href="mailto:amitfce@iitr.ac.in">amitfce@iitr.ac.in</a></p>
+                    <p><a href="http://faculty.iitr.ac.in/~amitfce/">http://faculty.iitr.ac.in/~amitfce/</a></p>
+                    <p><a href="https://civil.iitr.ac.in/CE?Uid=amitfce">https://civil.iitr.ac.in/CE?Uid=amitfce</a></p>
+                    </h3>
                 </div>
                 </div>
                 
--- src/Components/StartSuvey/Navigation/ NavigationItems/NavigationItems.js
+++ src/Components/StartSuvey/Navigation/ NavigationItems/NavigationItems.js
@@ -5,7 +5,9 @@ import {withRouter} from 'react-router-dom';
 const navigationItems =(props)=>(
     <ul className={classes.NavigationItems}>
         <NavigationItem anchor link="https://www.iitr.ac.in/">IIT Roorkee</NavigationItem>
-        <NavigationItem link={"/wiki"}>Wiki</NavigationItem>
+        <NavigationItem anchor link="https://civil.iitr.ac.in/">Civil Engineering</NavigationItem>
+        {/* <NavigationItem anchor link="https://civil.iitr.ac.in/TEG/about">Transportation Engineering</NavigationItem> */}
+        <NavigationItem link={"/wiki"}>Survey Wiki</NavigationItem>
         <NavigationItem link={"/privacypolicy"}>Privacy Policy</NavigationItem>
         <NavigationItem link={"/contact"}>Contact Us</NavigationItem>
     </ul>
